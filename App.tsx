94feaf19ed6b1c4e38a8a683b1c2f5d873be5d0f
WinterSunset95 <wintersunset95@gmail.com>
2022-12-03 19:21:05 +0530

    started the react native project

--- App.tsx
+++ App.tsx
@@ -1,10 +1,15 @@
+
+
+
+
 import { StatusBar } from 'expo-status-bar';
 import { StyleSheet, Text, View } from 'react-native';
+import Global from './src/pages/Global'
 
 export default function App() {
   return (
     <View style={styles.container}>
-      <Text>Open up App.tsx to start working on your app!</Text>
+			<Global />
       <StatusBar style="auto" />
     </View>
   );
---
+++ src/pages/Global.tsx
@@ -0,0 +1,15 @@
+
+
+
+
+
+
+import { StyleSheet, Text, View } from 'react-native'
+
+export default function Global() {
+	return (
+		<View>
+			<Text>This is the main page</Text>
+		</View>
+	)
+}
